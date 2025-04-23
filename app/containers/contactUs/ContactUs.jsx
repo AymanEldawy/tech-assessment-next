99738a485ba0ddf0e68ecaed0965233bb792a970
Ayman Eldawy <aymaneldawy@yahoo.com>
2025-04-23 13:44:19 +0200

    complete contact us

---
+++ app/containers/contactUs/ContactUs.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ContactAddress from './ContactAddress/ContactAddress'
+import ContactForm from './ContactForm/ContactForm'
+import styles from './ContactUs.module.scss';
+import ServiceMapBox from '../../components/shared/map/MapBox/ServiceMapBox';
+import { CONTACT_LOCATIONS } from '../../../data/DUMMY_DATA';
+import SectionWrapper from '../../components/shared/wrapper/SectionWrapper';
+
+const ContactUs = () => {
+  return (
+    <SectionWrapper>
+      <div className={styles.contact}>
+        <div className={styles.contact_map}>
+          <div className={styles.overlay}>
+            <ServiceMapBox center={[-80.6964, 43.550]} locations={CONTACT_LOCATIONS} markerClassName="contact_marker" />
+          </div>
+          <ContactAddress />
+        </div>
+        <ContactForm />
+      </div>
+    </SectionWrapper>
+  )
+}
+
+export default ContactUs
